4620b3ed263933d92374c69e11a357a62c748156
dhughes <dhughes2@statestreet.com>
2019-04-13 16:05:01 +0100

    added producer consumer code

--- app/app.py
+++ app/app.py
@@ -2,19 +2,20 @@ from flask import Flask
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import re
 import json
-from confluent_kafka import Consumer, KafkaError
+from confluent_kafka import Consumer
 from confluent_kafka import Producer
 
 CLEAN = re.compile("[^A-Z,a-z,\s]")
+
 
 def create_app(config=None, testing=False, cli=True):
     """
     Application factory, used to create application
     """
     app = Flask(__name__, static_folder=None)
-    
-    
+
     analyser = SentimentIntensityAnalyzer()
+
     def sentiment_analyzer_scores(sentence):
         """
         recieves
@@ -23,18 +24,20 @@ def create_app(config=None, testing=False, cli=True):
             clean_input = re.sub(CLEAN, "", sentence)
             score = analyser.polarity_scores(clean_input)
             return {"sentiment": score["compound"]}
-        except:
+        except Exception as exp:
             return False
 
-    c = Consumer({
-        'bootstrap.servers': 'localhost:9092',
-        'group.id': 'content_curator_twitter_group_12',
-        'auto.offset.reset': 'earliest'
-    })
+    c = Consumer(
+        {
+            "bootstrap.servers": "localhost:9092",
+            "group.id": "content_curator_twitter_group_12",
+            "auto.offset.reset": "earliest",
+        }
+    )
 
-    p = Producer({'bootstrap.servers': 'localhost:9092'})
+    p = Producer({"bootstrap.servers": "localhost:9092"})
 
-    c.subscribe(['content_curator_twitter'])
+    c.subscribe(["content_curator_twitter"])
 
     while True:
         msg = c.poll()
@@ -47,17 +50,20 @@ def create_app(config=None, testing=False, cli=True):
 
         # print('Received message: {}'.format(msg.value().decode('utf-8')))
         try:
-            m = json.loads(msg.value().decode('utf-8').replace("'", "\""))
-            seniment_value = str(sentiment_analyzer_scores(m["content"])).encode('utf-8')
+            m = json.loads(msg.value().decode("utf-8").replace("'", '"'))
+            seniment_value = str(sentiment_analyzer_scores(m["content"])).encode(
+                "utf-8"
+            )
             msg_key = msg.key()
 
             if msg_key is not None:
-                p.produce(topic='content_curator_twitter', key=msg_key, value=seniment_value)
+                p.produce(
+                    topic="content_curator_twitter", key=msg_key, value=seniment_value
+                )
                 p.flush()
-                print("ADDED", {"key": msg_key, "value": seniment_value})
+                print("ADDED:", {"key": msg_key, "value": seniment_value})
         except Exception as e:
-            a= 2
-            # print("ERROR:", e)
+            print("ERROR:", e)
 
     c.close()
 
