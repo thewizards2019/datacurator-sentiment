15eb5d77682a0fe724526bdc944425a691e7516f
kkeenan <kkeenan@statestreet.com>
2019-04-13 14:11:12 +0100

    fix: added functional code to sentiment tagger: JIRA Ref: Not specified

--- app/app.py
+++ app/app.py
@@ -1,15 +1,27 @@
 from flask import Flask
-
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import re
 # create_app wraps the other functions to set up the project
+
+CLEAN = re.compile("[^A-Z,a-z,\s]")
 
 def create_app(config=None, testing=False, cli=True):
     """
     Application factory, used to create application
     """
     app = Flask(__name__, static_folder=None)
-
-    @app.route("/")
-    def hello():
-        return "Hello World!"
+    
+    
+    analyser = SentimentIntensityAnalyzer()
+    def sentiment_analyzer_scores(sentence):
+        """
+        recieves
+        """
+        try:
+            clean_input = re.sub(pattern, "", sentence)
+            score = analyser.polarity_scores(clean_input)
+            return {"sentiment": score["compound"]}
+        except:
+            return False
 
     return app
