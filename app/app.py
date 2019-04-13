e8dad552298115cde7b022a193122ac8f33d5c37
dhughes <dhughes2@statestreet.com>
2019-04-13 19:43:14 +0100

    update

--- app/app.py
+++ app/app.py
@@ -53,7 +53,7 @@ def create_app(config=None, testing=False, cli=True):
             m = json.loads(msg.value().decode("utf-8"))
             if "content" in m.keys():
                 seniment_value = json.dumps(sentiment_analyzer_scores(m["content"]))
-                msg_key = str(msg.key())
+                msg_key = msg.key().decode('utf-8')
                 print("seniment_value", seniment_value)
                 if msg_key is not None:
                     p.produce(
